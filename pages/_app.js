a6b771d2f7cf28b2593944a43d1a68d207b4180d
Komori Yuichiro <mail.cmmmli@gmail.com>
2023-01-11 14:24:01 +0900

    chore: add test_user_id

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,32 @@
-import {useEffect} from "react"
+import { useEffect, useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import TagManager from "react-gtm-module";
+import { parseCookies, setCookie } from "nookies";
+
 import "../styles/globals.css";
 
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: "GTM-WLMMSC8",
-};
+const user_id_key = "test_user_id";
 
 function MyApp({ Component, pageProps }) {
+  let uuid = parseCookies().test_user_id;
+
+  if (uuid == null) {
+    uuid = uuidv4();
+    setCookie(null, user_id_key, uuid, { expires: 60 * 60 * 24 });
+  }
+
+  const tagManagerArgs = useMemo(() => {
+    return {
+      gtmId: "GTM-WLMMSC8",
+      dataLayer: {
+        userId: uuid,
+      },
+    };
+  }, [uuid]);
+
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
-  }, []);
+  }, [tagManagerArgs]);
 
   return <Component {...pageProps} />;
 }
